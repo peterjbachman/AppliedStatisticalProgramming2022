fa328b90ab934ce8cc249106c7eb338e4b8f32ed
Peter Bachman <peter.j.bachman@gmail.com>
2022-03-31 17:02:59 -0500

    Clean up integrateIt documentation

--- PS5/integrateIt/R/integrateIt.R
+++ PS5/integrateIt/R/integrateIt.R
@@ -2,14 +2,16 @@
 #'
 #' Calculates the integral estimation using either the Trapezoid rule or Simpson's rule
 #'
-#' @param x A numeric object
-#' @param y A numeric object with the same dimensionality as \code{x}.
+#' @param x Vector of X values, numeric
+#' @param y Vector of Y values in relation to \code{x}, numeric
+#' @param a Starting value found in \code{x}
+#' @param b Ending value found in \code{x}, numeric object
+#' @param rule Stating estimation rule to use. Either \code{"Trapezoid"} or \code{"Simpson"}
 #'
-#' @return An object of class AllSquares containing
-#'  \item{addSquares}{The sum of the squared values}
-#'  \item{subtractSquares}{The difference of the squared values}
-#'  \item{x}{The first object input}
-#'  \item{y}{The second object input}
+#' @return A list containing:
+#'  \item{EstType}{Estimation Class}
+#'  \item{Estimation}{Result of the estimation}
+#'  \item{Values}{Matrix of X and Y used in estimation}
 #' @author Peter Bachman
 #' @note This produces an object of a new class
 #' @examples
@@ -35,18 +37,20 @@ setMethod("integrateIt",
     .intervalY <- y[x >= a & x <= b]
     .n <- length(.intervalX) - 1
     .h <- (b - a) / .n
-
     # not sure why we don't separate the generic in the setMethod but oh well
     if (rule == "Trapezoid") {
+      # calculate the estimate
       .est <- (.h / 2) * (.intervalY[1] + sum(2 * .intervalY[2:(.n)]) + .intervalY[.n + 1])
       .class <- new("Trapezoid", x = .intervalX, y = .intervalY, a = a, b = b, n = .n, est = .est)
     } else if (rule == "Simpson") {
+      # calculate the estimate
+      # rep_len(c(4,2), .n - 1) flips between 4 and 2
       .est <- (.h / 3) * (.intervalY[1] + sum((rep_len(c(4,2), .n - 1) * .intervalY[2:(.n)])) + .intervalY[.n + 1])
       .class <- new("Simpson", x = .intervalX, y = .intervalY, a = a, b = b, n = .n, est = .est)
     } else {
       stop("Defined rule is not allowed. Use either Trapezoid or Simpson")
     }
-
-    return(list(.class, .est, matrix(c(x,y), ncol = 2), .h))
+    # Return the list of values requested
+    return(list(EstType = .class, Estimation = .est, Values = matrix(c(.class@x,.class@y), ncol = 2)))
   }
 )
