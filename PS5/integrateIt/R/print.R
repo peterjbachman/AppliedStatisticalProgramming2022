5a8f94f7fb578d63d22a1a5c97a27ee896ba99bd
Peter Bachman <peter.j.bachman@gmail.com>
2022-03-31 18:00:09 -0500

    Create Print function

---
+++ PS5/integrateIt/R/print.R
@@ -0,0 +1,17 @@
+#' @export
+setGeneric(
+  name = "print",
+  def = function(object) {
+    standardGeneric("print")
+  }
+)
+
+setMethod("print", "Trapezoid",
+          function(object){
+            return(object@est)
+          })
+
+setMethod("print", "Simpson",
+          function(object){
+            return(object@est)
+          })
