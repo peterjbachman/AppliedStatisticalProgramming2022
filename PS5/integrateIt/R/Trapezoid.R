be117eb74a40e3b19debe1e766693ba15b1b2678
Peter Bachman <peter.j.bachman@gmail.com>
2022-03-31 16:20:13 -0500

    Trapezoid Class update

--- PS5/integrateIt/R/Trapezoid.R
+++ PS5/integrateIt/R/Trapezoid.R
@@ -4,6 +4,8 @@
 #' @slot y vector of resulting values
 #' @slot a Start value of integration
 #' @slot b End value of integration
+#' @slot n number of intervals
+#' @slot est estimate of integral given the above information
 #' @rdname Trapezoid
 #' @export
 setClass(
@@ -12,13 +14,17 @@ setClass(
     x = "numeric",
     y = "numeric",
     a = "numeric",
-    b = "numeric"
+    b = "numeric",
+    n = "numeric",
+    est = "numeric"
   ),
   prototype = prototype(
     x = NA_real_,
     y = NA_real_,
     a = NA_real_,
-    b = NA_real_
+    b = NA_real_,
+    n = NA_real_,
+    est = NA_real_
   )
 )
 
