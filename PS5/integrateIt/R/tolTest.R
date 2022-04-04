9f91b2f22138c945985ed1f389e57bfad129c8bf
Peter Bachman <peter.j.bachman@gmail.com>
2022-04-04 08:04:02 -0500

    Comment out TolTest

--- PS5/integrateIt/R/tolTest.R
+++ PS5/integrateIt/R/tolTest.R
@@ -1,3 +1,6 @@
-tolTest <- function(fun, tol, rule, start, correct) {
 
-}
+# The instructions for this were unclear, I wasn't sure what was expected for
+# this function
+
+# tolTest <- function(fun, tol, rule, start, correct) {
+# }
