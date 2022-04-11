0dc316f938742ad1dcb03b6d2a8764933c608bc7
Peter Bachman <peter.j.bachman@gmail.com>
2022-04-11 16:39:10 -0500

    Add check() to make sure the package has valid checks

--- midterm/easyPoisDevelopment.R
+++ midterm/easyPoisDevelopment.R
@@ -1,13 +1,16 @@
 # This script comes from the devtoolsIntro from class
 # This correctly runs when the project is set to the easyPois package
-setwd("../") # Run once
+setwd("../") # Run for directory above current
 
 ## Load libraries and set working directory
 library(devtools)
 library(roxygen2)
 library(testthat)
 
-## This can be run many times as the code is updates
+## This can be run many times as the code updates
 current.code <- as.package("easyPois")
 load_all(current.code)
 document(current.code)
+
+# Check the
+check("./easyPois")
