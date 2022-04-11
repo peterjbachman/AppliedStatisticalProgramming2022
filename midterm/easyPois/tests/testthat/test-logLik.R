5c8c1eebd7c59d0d4eac73c01568278fb8bd55fa
Peter Bachman <peter.j.bachman@gmail.com>
2022-04-11 15:24:50 -0500

    Test logLik function

--- midterm/easyPois/tests/testthat/test-logLik.R
+++ midterm/easyPois/tests/testthat/test-logLik.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Log likelihood is calculated correctly", {
+  set.seed(12345)
+  y <- rpois(n = 10000, lambda = 5)
+
+  # without the tolerance argument this fails
+  expect_equal(logLik(y, 5), -21967.24, tolerance = 0.0001)
 })
