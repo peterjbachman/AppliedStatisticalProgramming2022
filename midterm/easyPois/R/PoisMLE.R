224de0136308509d1bbf3c9dddd964b4a96e7ec6
Peter Bachman <peter.j.bachman@gmail.com>
2022-04-11 17:24:56 -0500

    Add validation checks to S4 Class

--- midterm/easyPois/R/PoisMLE.R
+++ midterm/easyPois/R/PoisMLE.R
@@ -27,8 +27,20 @@ setClass(
 
 setValidity("PoisMLE", function(object) {
   test1 <- object@SEType == "basic" | object@SEType == "bootstrap"
+  test2 <- object@MLE == mle(object@y)
+  test3 <- object@LL == logLik(object@y, object@MLE)
+  test4 <- if (object@SEType == "basic") {object@SE == standardError(object@y, "basic")} else {TRUE}
   if (!test1) {
     stop("@SEType needs to be either \'basic\' or \'bootstrap\'")
+  }
+  if (!test2) {
+    stop("MLE not correct")
+  }
+  if (!test3) {
+    stop("Log Likelihood is not correct")
+  }
+  if (!test4) {
+    stop("SEType is \"basic\" and is not correct.")
   }
 })
 
