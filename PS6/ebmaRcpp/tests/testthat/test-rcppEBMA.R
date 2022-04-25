88cead518f5d8b9afcaaa5e14e43df36b1da7fcf
Peter Bachman <peter.j.bachman@gmail.com>
2022-04-25 17:04:14 -0500

    add tests

--- PS6/ebmaRcpp/tests/testthat/test-rcppEBMA.R
+++ PS6/ebmaRcpp/tests/testthat/test-rcppEBMA.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("full function works", {
+  y <- rep_len(50, 30)
+  weights <- rep_len(1 / 7, 7)
+  expect_equal(
+    rCppEBMA(as.matrix(attitude), y, weights, 5, tolerance = 0.00005),
+    c(
+      3.349082e-02, 8.127623e-11, 3.768975e-01, 2.734239e-01, 3.751721e-02,
+      2.835489e-30, 2.786706e-01
+    ), tolerance = 0.0001
+  )
 })
--- PS6/ebmaRcpp/tests/testthat/test-zHat.R
+++ PS6/ebmaRcpp/tests/testthat/test-zHat.R
@@ -1,3 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Calculations are done correctly", {
+  expect_error(
+    zHat(c(1, 2, 3, 4, 5), 4, 1, 2),
+    "Not a matrix."
+  )
+  expect_equal(
+    zHat(as.matrix(attitude[1:3, ]), rep.int(50, 30), rep_len(1 / 7, 7), 1),
+    matrix(c(
+      3.775111e-11, 9.999939e-01, 2.281646e-87, 8.756457e-27,
+      8.756457e-27, 0.000000e+00, 6.144175e-06, 3.244481e-37,
+      4.448051e-43, 9.814807e-01, 5.428416e-04, 3.244481e-37,
+      2.178405e-115, 1.797645e-02, 1.278367e-95, 1.022569e-86,
+      3.257489e-70, 3.009089e-78, 1.194137e-146, 2.791031e-281,
+      1.000000e+00
+    ),
+    nrow = 3, ncol = 7, byrow = TRUE
+    ),
+    tolerance = 0.0001
+  )
 })
