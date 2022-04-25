8e6212c39e890e4a01419170124bcaddbf323925
Peter Bachman <peter.j.bachman@gmail.com>
2022-04-25 17:04:01 -0500

    move development file to the right place

---
+++ PS6/dev.R
@@ -0,0 +1,14 @@
+df <- EBMAforecast::presidentialForecast
+x <- df[,1:6]
+y <- df[,7]
+wHat <- replicate(dim(x)[2],1/dim(x)[2])
+
+
+zHat <- ebmaRcpp::zHat(as.matrix(x), y, wHat, 1)
+zHat
+
+weights <- ebmaRcpp::wHat(zHat)
+
+rCppEBMA(as.matrix(x), y, wHat, 1, 2)
+
+dnorm(44.5948, 0.0192778, 1, FALSE)
