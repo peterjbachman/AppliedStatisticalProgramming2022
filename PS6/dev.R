e4d2ad2a079c9b82c05b5e6ba69327b324a1ace8
Peter Bachman <peter.j.bachman@gmail.com>
2022-04-25 17:29:11 -0500

    Remove feable attempt in dev file

--- PS6/dev.R
+++ PS6/dev.R
@@ -12,3 +12,6 @@ weights <- ebmaRcpp::wHat(zHat)
 rCppEBMA(as.matrix(x), y, wHat, 1, 2)
 
 dnorm(44.5948, 0.0192778, 1, FALSE)
+
+for (i in 1:5)
+weightsNew <- rCppEBMA(as.matrix(attitude), y, weights, 5, tolerance = 0.005)
